388efd46a2ef5d217fafdbdd1913c812c3c0ca8a
gritse <24323376+gritse@users.noreply.github.com>
2026-04-09 21:43:09 +0200

    dotenv-crypt 0.2

--- Formula/dotenv-crypt.rb
+++ Formula/dotenv-crypt.rb
@@ -1,8 +1,8 @@
 class DotenvCrypt < Formula
   desc "Encrypt individual values in .env files using AES-256-GCM with macOS Keychain"
   homepage "https://github.com/gritse/dotenv-crypt"
-  url "https://github.com/gritse/dotenv-crypt/archive/refs/tags/v0.1.tar.gz"
-  sha256 "d3364670f3ab5cebb7f76ebc16f6961d24744efc23000cfab8fc3aaed0410737"
+  url "https://github.com/gritse/dotenv-crypt/archive/refs/tags/v0.2.tar.gz"
+  sha256 "6fa45abd9e77ea1d36daaf371f41750c680374be3ab277d85dcdd02d6ad767bf"
   license "GPL-2.0-only"
 
   depends_on xcode: ["15.0", :build]
